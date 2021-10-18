742589a8731591e646a3c71bc7d6389ee36b9fc8
pini85 <pinchas.hodadad@gmail.com>
2021-10-18 10:25:55 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import IFrame from "./Iframe";
+import React, { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    console.log("localstroage value", localStorage.getItem("id"));
+  }, []);
   return (
     <div style={{ height: "80vh" }}>
       <IFrame></IFrame>
