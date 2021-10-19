02c714242d39d1f706e096c7335fb93d1b49f697
pini85 <pinchas.hodadad@gmail.com>
2021-10-19 17:51:48 +0300

    A

--- src/Iframe.js
+++ src/Iframe.js
@@ -32,7 +32,7 @@ const IFrame = () => {
           })
           .then((_) => {
             console.log("Now we have first-party storage access!");
-            document.cookie = `${c[0]}=${c[1]}; SameSite=none; Secure; Domain=https://server-1.netlify.app/`;
+            document.cookie = `${c[0]}=${c[1]}; SameSite=none; Secure;`;
 
             console.log(`document.cookie: ${document.cookie}`);
           })
