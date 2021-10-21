33b351371ff4781f6015dcd121bb1b7dd84c81f2
pini85 <pinchas.hodadad@gmail.com>
2021-10-21 15:07:00 +0300

    a

--- src/Iframe.js
+++ src/Iframe.js
@@ -4,54 +4,54 @@ const IFrame = () => {
   const [iData, setIData] = useState("");
   const [cookieData, setCookieData] = useState(null);
 
-  useEffect(() => {
-    const handler = (e) => {
-      //! security
-      // check origin
+  // useEffect(() => {
+  //   const handler = (e) => {
+  //     //! security
+  //     // check origin
 
-      if (
-        e.origin === "https://server-2.netlify.app" ||
-        e.origin === "http://localhost:3001"
-      ) {
-        console.log(`origin is ${e.origin}`);
+  //     if (
+  //       e.origin === "https://server-2.netlify.app" ||
+  //       e.origin === "http://localhost:3001"
+  //     ) {
+  //       console.log(`origin is ${e.origin}`);
 
-        //check message
-        const data = JSON.parse(e.data);
-        console.log("data", data.message);
-        const c = data.message.split("=");
-        console.log("c", c[0], c[1]);
+  //       //check message
+  //       const data = JSON.parse(e.data);
+  //       console.log("data", data.message);
+  //       const c = data.message.split("=");
+  //       console.log("c", c[0], c[1]);
 
-        document.cookie = `${c[0]}=${c[1]}; SameSite=none; Secure;`;
-        document
-          .hasStorageAccess()
-          .then((hasAccess) => {
-            console.log("hasAccess: " + hasAccess);
-            if (!hasAccess) {
-              return document.requestStorageAccess();
-            }
-          })
-          .then((_) => {
-            console.log("Now we have first-party storage access!");
-            // document.cookie = `dfgdfgdfgdfgdfg=${c[1]}; SameSite=none; Secure;`;
+  //       document.cookie = `${c[0]}=${c[1]}; SameSite=none; Secure;`;
+  //       document
+  //         .hasStorageAccess()
+  //         .then((hasAccess) => {
+  //           console.log("hasAccess: " + hasAccess);
+  //           if (!hasAccess) {
+  //             return document.requestStorageAccess();
+  //           }
+  //         })
+  //         .then((_) => {
+  //           console.log("Now we have first-party storage access!");
+  //           // document.cookie = `dfgdfgdfgdfgdfg=${c[1]}; SameSite=none; Secure;`;
 
-            console.log(`document.cookie: ${document.cookie}`);
-          })
-          .catch((_) => {
-            console.log("error");
-          });
-      }
-    };
+  //           console.log(`document.cookie: ${document.cookie}`);
+  //         })
+  //         .catch((_) => {
+  //           console.log("error");
+  //         });
+  //     }
+  //   };
 
-    window.addEventListener("message", handler);
-    return () => {
-      window.removeEventListener("message", handler);
-    };
-  }, []);
+  //   window.addEventListener("message", handler);
+  //   return () => {
+  //     window.removeEventListener("message", handler);
+  //   };
+  // }, []);
   const url = () => {
     if (process.env.NODE_ENV === "development") {
       return "http://localhost:3001/";
     }
-    return "https://server-2.netlify.app/";
+    return "https://fake-meta-app.herokuapp.com/";
   };
   return (
     <div>
