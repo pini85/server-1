f4049550c1897adb4c480c7384d3d9dadfc23c40
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2021-11-16 14:29:19 +0200

    A

--- src/Iframe.js
+++ src/Iframe.js
@@ -1,62 +1,34 @@
 import React, { useEffect, useState } from "react";
 
 const IFrame = () => {
-  const [iData, setIData] = useState("");
+  const [isEnabled, setIsEnabled] = useState(false);
   const [cookieData, setCookieData] = useState(null);
 
-  // useEffect(() => {
-  //   const handler = (e) => {
-  //     //! security
-  //     // check origin
+  useEffect(() => {
+    const socket = new WebSocket(`ws://${url()}`);
+    socket.addEventListener("open", function (event) {
+      console.log("Connected to WS Server from server1");
+    });
+    socket.addEventListener("message", function (event) {
+      console.log("Message from server ", event.data);
+      setIsEnabled(true);
+    });
+  }, []);
 
-  //     if (
-  //       e.origin === "https://server-2.netlify.app" ||
-  //       e.origin === "http://localhost:3001"
-  //     ) {
-  //       console.log(`origin is ${e.origin}`);
-
-  //       //check message
-  //       const data = JSON.parse(e.data);
-  //       console.log("data", data.message);
-  //       const c = data.message.split("=");
-  //       console.log("c", c[0], c[1]);
-
-  //       document.cookie = `${c[0]}=${c[1]}; SameSite=none; Secure;`;
-  //       document
-  //         .hasStorageAccess()
-  //         .then((hasAccess) => {
-  //           console.log("hasAccess: " + hasAccess);
-  //           if (!hasAccess) {
-  //             return document.requestStorageAccess();
-  //           }
-  //         })
-  //         .then((_) => {
-  //           console.log("Now we have first-party storage access!");
-  //           // document.cookie = `dfgdfgdfgdfgdfg=${c[1]}; SameSite=none; Secure;`;
-
-  //           console.log(`document.cookie: ${document.cookie}`);
-  //         })
-  //         .catch((_) => {
-  //           console.log("error");
-  //         });
-  //     }
-  //   };
-
-  //   window.addEventListener("message", handler);
-  //   return () => {
-  //     window.removeEventListener("message", handler);
-  //   };
-  // }, []);
   const url = () => {
     if (process.env.NODE_ENV === "development") {
-      return "http://localhost:3001/";
+      return "localhost:8080/";
     }
-    return "https://fake-meta-app.herokuapp.com/";
+    return "pini-backend-playground.herokuapp.com/";
   };
+
   return (
     <div>
-      <iframe style={{ height: "80vh", width: "100%" }} src={url()} />
-      <div>{iData}</div>
+      <iframe
+        style={{ height: "80vh", width: "100%" }}
+        src="https://server-2.netlify.app/"
+      />
+      <button disabled={isEnabled}>Continue</button>
     </div>
   );
 };
